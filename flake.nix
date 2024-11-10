f3183dc9537c5b3624317fc26fbfc3e201ace676
ken <kenken17@gmail.com>
2024-11-10 13:17:10 +0800

    Remove the mac setup

--- flake.nix
+++ flake.nix
@@ -3,76 +3,58 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
-    packages = {
-      x86_64-linux.default =
-        let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          allPackages = with pkgs; [
-            awscli
-            bash-completion
-            bat
-            curl
-            expect
-            eza
-            file
-            fzf
-            gcc
-            git
-            git-extras
-            gnome-tweaks
-            httpie
-            jq
-            lua
-            luarocks
-            maven
-            neovim
-            nodejs_18 # Node.js 18, plus npm, npx, and corepack
-            python3
-            python312Packages.pip
-            ripgrep
-            slides
-            starship
-            stow
-            tmux
-            wget
-            xsel
-            zoxide
-          ];
-        in
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      allPackages = with pkgs; [
+        awscli
+        bash-completion
+        bat
+        curl
+        expect
+        eza
+        file
+        fzf
+        gcc
+        git
+        git-extras
+        gnome-tweaks
+        httpie
+        jq
+        lua
+        luarocks
+        maven
+        neovim
+        nodejs_18 # Node.js 18, plus npm, npx, and corepack
+        python3
+        python312Packages.pip
+        ripgrep
+        slides
+        starship
+        stow
+        tmux
+        wget
+        xsel
+        zoxide
+      ];
+    in
+    {
+      packages = {
+        ${system}.default =
+          # Define a package environment as a profile
+          pkgs.buildEnv {
+            name = "ken-profile";
+            paths = allPackages;
+          };
+      };
 
-        # Define a package environment as a profile
-        pkgs.buildEnv {
-          name = "ken-profile";
-          paths = allPackages;
-        };
-    };
-
-    devShells =
-      let
-        systemLinux = "x86_64-linux";
-        systemMac = "aarch64-darwin";
-      in
-      {
-        ${systemLinux}.default = nixpkgs.legacyPackages.${systemLinux}.mkShell {
-          buildInputs =
-            let
-              pkgs = nixpkgs.legacyPackages.${systemLinux};
-            in
-            [
-              pkgs.ripgrep
-            ];
-        };
-
-        ${systemMac} = nixpkgs.legacyPackages.${systemMac}.mkShell {
-          buildInputs =
-            let
-              pkgs = nixpkgs.legacyPackages.${systemLinux};
-            in
-            [
-              pkgs.ripgrep
-            ];
+      devShell = {
+        ${system} = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = allPackages;
         };
       };
-  };
+    };
 }
